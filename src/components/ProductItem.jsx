f93f38a6c7feef31a9ed53e725531cd0c89fb44c
gimibond <macaletz@gmail.com>
2022-03-19 10:18:59 +0100

    react useState

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '@styles/ProductItem.scss';
 
 import bt_add_to_cart from '@icons/bt_add_to_cart.svg';
 
 const ProductItem = () => {
+	const [cart, setCart] = useState('');
+
+	const handleClick = () => {
+		setCart('Hola Sorine');
+	}
 	return (
 		<div className="ProductItem">
 			<img src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="" />
@@ -12,9 +17,10 @@ const ProductItem = () => {
 					<p>$120,00</p>
 					<p>Bike</p>
 				</div>
-				<figure>
-					<img src={bt_add_to_cart} alt="" />
+				<figure onClick={handleClick}>
+					<img src={bt_add_to_cart} alt="Add to cart" />
 				</figure>
+				{cart}
 			</div>
 		</div>
 	);
